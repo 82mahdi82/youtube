1687b2b48b6b54f0fcd0e245d0f3fd7b2f2a5f51
82mahdi82 <141176041+82mahdi82@users.noreply.github.com>
2024-02-25 12:57:05 +0330

    bbbbb

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from pyrogram import Client
 # تنظیمات API
 api_id = 27075414
 api_hash = "aca0fe0dc1fae5c69309a8c04f487cde"
+phone_number ="+989384245687"
 
 # ایجاد یک نمونه از Client
-app = Client("my", api_id=api_id, api_hash=api_hash)
+app = Client("my", api_id=api_id, api_hash=api_hash, phone_number=phone_number)
 
 # تعریف تابع برای دریافت پیام
 @app.on_message()
